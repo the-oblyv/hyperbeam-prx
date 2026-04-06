482072bb5001c3cd1c32e1946a2ec84c98fb8831
Oblyv <gregrmcbride@gmail.com>
2026-04-06 17:14:09 -0400

    Update create-session.js

--- api/create-session.js
+++ api/create-session.js
@@ -6,9 +6,10 @@ export default async function handler(req, res) {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      timeout: 300000,
-      region: "US"
-    })
+  timeout: 300000,
+  region: "US",
+  start_url: "https://searx.tiekoetter.com/"
+})
   });
 
   const data = await response.json();
