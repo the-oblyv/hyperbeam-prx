d0035c4b527423d8a968364888dbb5251edec409
Oblyv <gregrmcbride@gmail.com>
2026-04-06 18:40:07 -0400

    Create imports.js

---
+++ imports.js
@@ -0,0 +1,3 @@
+// imports.js
+export { auth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+export { Hyperbeam } from "https://cdn.hyperbeam.com/hyperbeam.min.js";
